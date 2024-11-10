050348d8c5f52f20cc0f4b358571ab113ddb4f8a
Gavin Hill <alexandergavin33@gmail.com>
2024-11-09 23:32:51 -0500

    gavin push

--- frontend/frontendJS.js
+++ frontend/frontendJS.js
@@ -14,6 +14,7 @@ function showScreen(screenId) {
         document.querySelector('#Justin').style.display = 'none';
     }
 }
+
 fetch(`/fighters/${fighterName}`)
     .then(response => response.json())
     .then(data => {
